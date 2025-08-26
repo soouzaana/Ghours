ab04451ae73bcba2b389125b4cd0b0ebbb16a2af
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-08-25 20:18:25 -0400

    feat: finalize sidebar implementation :sparkles:

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script lang="ts" setup>
 import { ref } from "vue";
 import Home from "../Icons/Home.vue";
 import Report from "../Icons/Report.vue";
@@ -9,9 +9,7 @@ import Peoples from "../Icons/Peoples.vue";
 import Settings from "../Icons/Settings.vue";
 import Exit from "../Icons/Exit.vue";
 
-//itens da sidebar
-const source = ref([
-  { icon: Hamburguer, label: "Menu" },
+const topItems = ref([
   { icon: Home, label: "Início" },
   { icon: Report, label: "Relatórios" },
   { icon: AddPeople, label: "Clientes" },
@@ -19,91 +17,150 @@ const source = ref([
   { icon: Peoples, label: "Funcionários" },
 ]);
 
-//itens da sidebar bottom
-const bottomSource = ref([
+const bottomItems = ref([
   { icon: Settings, label: "Configurações" },
   { icon: Exit, label: "Sair" },
 ]);
+
+const activeIndex = ref<number | null>(null);
+const isExpanded = ref(false);
+const isHover = ref(false);
+
+const setActive = (index: number) => {
+  activeIndex.value = index;
+};
+
+const toggleSidebar = () => {
+  isExpanded.value = !isExpanded.value;
+};
 </script>
 
 <template>
-  <nav class="sidebar">
-    <ul>
-      <li v-for="(item, index) in source" :key="index" class="sidebar-item">
-        <span class="icon">
-          <template v-if="typeof item.icon === 'string'">{{
-            item.icon
-          }}</template>
-          <component v-else :is="item.icon" />
-        </span>
-      </li>
+  <nav
+    class="sidebar"
+    @mouseenter="isHover = true"
+    @mouseleave="isHover = false"
+    :class="{ expanded: isExpanded || isHover }"
+  >
+    <!-- botão de toggle -->
+    <div class="top-section">
+      <div class="toggle" @click="toggleSidebar">
+        <component :is="Hamburguer" class="icon" />
+      </div>
 
+      <ul class="top">
+        <li
+          v-for="(item, index) in topItems"
+          :key="index"
+          class="sidebar-item"
+          :class="{ active: activeIndex === index }"
+          @click="setActive(index)"
+        >
+          <component :is="item.icon" class="icon" />
+          <span class="label">{{ item.label }}</span>
+        </li>
+      </ul>
+    </div>
+
+    <ul class="bottom">
       <li
-        v-for="(item, index) in bottomSource"
+        v-for="(item, index) in bottomItems"
         :key="index"
-        class="sidebar-bottom"
+        class="sidebar-item"
       >
-        <span class="icon">
-          <template v-if="typeof item.icon === 'string'">{{
-            item.icon
-          }}</template>
-          <component v-else :is="item.icon" />
-        </span>
+        <component :is="item.icon" class="icon" />
+        <span class="label">{{ item.label }}</span>
       </li>
     </ul>
   </nav>
 </template>
 
-<style>
+<style scoped>
 .sidebar {
-  width: 1rem;
-  height: 100vh;
   position: fixed;
   top: 0;
   left: 0;
+  bottom: 0;
+  width: 80px;
+  height: 100vh;
   background-color: #e39b34;
-  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px 5px;
   box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
-  overflow-y: auto;
+  overflow: hidden;
+  transition: width 0.5s ease;
 }
 
-.sidebar ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
+.sidebar.expanded {
+  width: 200px;
+}
+
+.toggle {
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
+}
+
+.top {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 10px;
+  gap: 10px;
+}
+
+.bottom {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 5px;
 }
 
 .sidebar-item {
   display: flex;
-  margin-bottom: 10px;
-  padding: 1rem 0.5rem;
-  cursor: pointer;
-  border-radius: 8px;
-  transition: background 0.2s;
-}
-
-.sidebar-bottom {
-  display: flex;
-  align-items: center;
+  align-items: flex-start;
   padding: 10px;
-  margin-top: 2rem;
   cursor: pointer;
-  border-radius: 8px;
   transition: background 0.2s;
 }
 
 .sidebar-item:hover {
-  background-color: rgba(0, 0, 0, 0.1);
+  background-color: #808080;
+  border-radius: 10px;
+}
+
+.active {
+  background-color: #808080;
+  border-radius: 10px;
 }
 
 .icon {
-  margin-right: 10px;
-  font-size: 1rem;
-  color: #f7f7f7;
+  font-size: 24px;
+  color: #fff;
+  flex-shrink: 0;
 }
 
 .label {
-  font-weight: bold;
-  color: #191817;
+  font-size: 14px;
+  color: #fff;
+  margin-left: 10px;
+  font-weight: 500;
+
+  max-width: 0;
+  opacity: 0;
+  overflow: hidden;
+  white-space: nowrap;
+  transition: max-width 0.5s ease, opacity 0.5s ease;
+}
+
+.sidebar.expanded .label {
+  max-width: 200px;
+  opacity: 1;
+}
+
+ul {
+  padding-inline-start: 20px;
 }
 </style>
