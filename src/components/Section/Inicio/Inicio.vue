6176b4b4163e28a6f5d1141775c0b6ed5245e0d2
Gabriella Osório Ribeiro <gabriellaribeiro0934@gmail.com>
2025-08-28 09:45:05 -0400

    feat: add new section to Inicio component

--- src/components/Section/Inicio/Inicio.vue
+++ src/components/Section/Inicio/Inicio.vue
@@ -6,5 +6,6 @@ import MainLayout from "../../MainLayout/MainLayout.vue";
   <MainLayout>
     <h1 class="text-3xl font-bold mb-4">Página Inicial</h1>
     <p>gi</p>
+    <div>adicionando nova seção</div>
   </MainLayout>
 </template>
