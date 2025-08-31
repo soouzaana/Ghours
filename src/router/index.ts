ba417a05d2191a07314160a55e22ec4c96da609d
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-08-31 18:20:42 -0400

    feat: add routing for Relatorios, Clientes, and Funcionarios; :sparkles:

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Inicio from "../components/Section/Inicio/Inicio.vue";
 import Agendamentos from "../components/Section/Agendamentos/Agendamento.vue";
+import Relatorios from "../components/Section/Relatorios/Relatorios.vue";
+import Clientes from "../components/Section/Clientes/Clientes.vue";
+import Funcionarios from "../components/Section/Funcionarios/Funcionarios.vue";
+import path from "path";
 
 const routes = [
   { path: "/", component: Inicio },
+  { path: "/relatorios", component: Relatorios },
+  { path: "/clientes", component: Clientes },
   { path: "/agendamentos", component: Agendamentos },
+  { path: "/funcionarios", component: Funcionarios },
 ];
 
 const router = createRouter({
